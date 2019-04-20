ee5baa8cfa9f263dd5401ce8f5db892941cf090b
nelsons <nelson.simao@me.com.br>
2019-04-20 17:58:59 -0300

    - EnableConfigServer annotation - Connect the application to the git repository configuration. https://github.com/nsimao/microservices-spring-cloud-config-repo

--- spring-cloud-config-server/src/main/java/com/nsimao/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/com/nsimao/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -2,12 +2,14 @@ package com.nsimao.microservices.springcloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class SpringCloudConfigServerApplication {
+// --------------------------- main() method ---------------------------
 
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudConfigServerApplication.class, args);
     }
-
 }
