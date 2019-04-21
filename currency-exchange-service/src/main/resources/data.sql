761a48a5d6ad0ed07374aab931d9625fd3ba7588
nelsons <nelson.simao@me.com.br>
2019-04-21 12:41:45 -0300

    - New data conversion.

--- currency-exchange-service/src/main/resources/data.sql
+++ currency-exchange-service/src/main/resources/data.sql
@@ -1,4 +1,8 @@
 insert into exchange_value(currency_from,currency_to,conversion_multiple)
-values('USD','BRL',3.95);
+values('USD','BRL',3.94);
 insert into exchange_value(currency_from,currency_to,conversion_multiple)
-values('EUR','BRL',4.2);
+values('BRL','USD',0.25);
+insert into exchange_value(currency_from,currency_to,conversion_multiple)
+values('EUR','BRL',4.43);
+insert into exchange_value(currency_from,currency_to,conversion_multiple)
+values('BRL','USD',0.23);
