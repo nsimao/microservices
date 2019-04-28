1ad1719c28f3abd9771403e8d2d88ccf90c51b34
nelsons <nelson.simao@me.com.br>
2019-04-28 17:48:19 -0300

    - Creating Eureka Server Application.

---
+++ netflix-eureka-naming-server/src/main/java/com/nsimao/microservices/netflixeurekanamingserver/NetflixEurekaNamingServerApplication.java
@@ -0,0 +1,15 @@
+package com.nsimao.microservices.netflixeurekanamingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class NetflixEurekaNamingServerApplication {
+// --------------------------- main() method ---------------------------
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetflixEurekaNamingServerApplication.class, args);
+    }
+}
