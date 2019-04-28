2522c80e91fb8d81d3ab8b865a14cc7cdb6001b7
nelsons <nelson.simao@me.com.br>
2019-04-28 19:14:36 -0300

    Changed the FeignClient annotation from the currency-exchange-service to go through the Zull Proxy netflix-zuul-api-gateway-server.

--- currency-conversion-service/src/main/java/com/nsimao/microservices/currencyconversionservice/proxy/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/nsimao/microservices/currencyconversionservice/proxy/CurrencyExchangeServiceProxy.java
@@ -11,11 +11,11 @@ import org.springframework.web.bind.annotation.PathVariable;
  * @since 0.1
  */
 
-@FeignClient(name = "currency-exchange-service")
+@FeignClient(name = "netflix-zuul-api-gateway-server")
 @RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 // -------------------------- OTHER METHODS --------------------------
 
-    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    @GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
     ExchangeValueCommand getExchangeValue(@PathVariable String from, @PathVariable String to);
 }
